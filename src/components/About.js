cc4069c5bd86d4c38af5d211e578e5711778b2a6
Tatiana Smolin <tatismolin@gmail.com>
2020-02-20 17:07:23 -0700

    Adds functionality to buttons.

--- src/components/About.js
+++ src/components/About.js
@@ -38,7 +38,7 @@ class About extends Component{
                 <Navigation handleClick={this.handleDrawerClick} />
                 <DropDown show={dropDownOpen} />   
                 {backDrop}
-                <div className="about">
+                <div className="about" id="section1">
                     <h1>ABOUT</h1>
                     <div className="description">
                         <img className="profile-image" src={ProfileImage} alt="profile"></img>
--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-scroll";
 
 import "../styles/DropDown.css";
 
@@ -9,11 +10,46 @@ function dropDown(props){
     };
     return(
         <div className={drawerClasses}>
+            <Link
+                to="section0" 
+                activeClass="active"
+                spy={true}
+                smooth={true}
+                duration={1000}>
             <h2 className="drop-down-text">HOME</h2>
+            </Link>
+            <Link
+                to="section1" 
+                activeClass="active"
+                spy={true}
+                smooth={true}
+                duration={1000}>
             <h2 className="drop-down-text">ABOUT</h2>
+            </Link>
+            <Link
+                to="section2" 
+                activeClass="active"
+                spy={true}
+                smooth={true}
+                duration={1000}>
             <h2 className="drop-down-text">PORTFOLIO</h2>
+            </Link>
+            <Link
+                to="section3" 
+                activeClass="active"
+                spy={true}
+                smooth={true}
+                duration={1000}>
             <h2 className="drop-down-text">BLOG</h2>
+            </Link>
+            <Link
+                to="section4" 
+                activeClass="active"
+                spy={true}
+                smooth={true}
+                duration={1000}>
             <h2 className="drop-down-text">CONTACT</h2>
+            </Link>
         </div>
     );
 };
